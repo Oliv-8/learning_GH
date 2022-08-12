a9a159e83fa0fd10ab914c6727da5f75c3999039
Oliv-8 <foxbelno@tutanota.com>
2022-08-12 16:00:06 +0200

    Initial Commit

---
+++ learning GitHub/learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_GitHubApp.swift
+//  learning GitHub
+//
+//  Created by Laurent Lebon on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
